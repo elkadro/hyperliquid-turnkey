0513e46d2d5bb8bb0824d49286c7e31aa6797129
elkadro <ahmadkaderi@gmail.com>
2024-11-27 07:49:27 -0400

    changed to axios post

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,24 +4,24 @@ import { RateLimiter } from './rateLimiter';
 
 
 export class HttpApi {
-    private client: AxiosInstance;
     private endpoint: string;
     private rateLimiter: RateLimiter;
     private proxy: string | undefined = undefined;
+    private theBase: string = "";
 
     constructor(baseUrl: string, endpoint: string = "/", rateLimiter: RateLimiter, _proxy?: string) {
         if(_proxy) {
             this.proxy = _proxy;
         }
-        const base = (this.proxy && this.proxy.length > 0) ? this.proxy : baseUrl;
-        console.log(`Hyperliquid SDK: Using url ${base}`);
+        this.theBase = (this.proxy && this.proxy.length > 0) ? this.proxy : baseUrl;
+        console.log(`Hyperliquid SDK: Using url ${this.theBase}`);
         this.endpoint = endpoint;
-        this.client = axios.create({
-            baseURL: base,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+        // this.client = axios.create({
+        //     baseURL: base,
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        // });
         this.rateLimiter = rateLimiter;
     }
 
@@ -30,9 +30,16 @@ export class HttpApi {
             
             await this.rateLimiter.waitForToken();
             const past = Date.now();
-            const response = await this.client.post(endpoint, payload);
+            const response = await axios.post(`${this.theBase}${endpoint}`, payload, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            // const response = await this.client.post(endpoint, payload);
             console.log(`Hyperliquid SDK: Request of ${endpoint} took ${Date.now() - past}ms`);
+            
             return response.data;
+            
         } catch (error) {
             handleApiError(error);
         }
